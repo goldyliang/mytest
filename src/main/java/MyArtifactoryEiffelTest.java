47a37a268d0034dc6e3094b64711b72478e5a4ce
eshibig <bing.a.shi@ericsson.com>
2014-08-04 17:38:41 -0400

    add test src

---
+++ src/main/java/MyArtifactoryEiffelTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by eshibig on 04/08/2014.
+ */
+public class MyArtifactoryEiffelTest {
+
+    public MyArtifactoryEiffelTest() {
+    }
+
+    public int add2numbers(int x, int y) {
+        System.out.println("Sum=" + String.valueOf(x + y));
+        return x+y;
+    }
+}
